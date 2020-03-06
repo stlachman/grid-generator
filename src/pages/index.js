738a3d89cc94d5d9de9984c433f17cfede5a05ca
Seth Lachman <lachmanseth17@gmail.com>
2020-03-06 11:20:49 -0500

    Updated index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,17 @@ const Container = styled.div`
   justify-content: space-around;
 `;
 
+const InputGrid = styled.section`
+  display: grid;
+`;
+
+const InputRow = styled.section`
+  display: grid;
+  height: 100%;
+  position: relative;
+  left: -30px;
+`;
+
 const Flex = styled.div`
   display: flex;
 `;
@@ -53,8 +64,8 @@ function expandArray(len) {
 
 const IndexPage = () => {
   const [grid, setGrid] = useState([...Array(25).keys()]);
-  const [rows, setRows] = useState(5);
-  const [columns, setColumns] = useState(5);
+  const [rows, setRows] = useState(["1fr", "1fr", "1fr", "1fr", "1fr"]);
+  const [columns, setColumns] = useState(["1fr", "1fr", "1fr", "1fr", "1fr"]);
   const [columnGap, setColumnGap] = useState(0);
   const [rowGap, setRowGap] = useState(0);
   const [show, setShow] = useState(false);
@@ -63,25 +74,26 @@ const IndexPage = () => {
 
   const changeColumns = e => {
     let num = Number(e.target.value);
-    if (num > columns) {
+    if (num > columns.length) {
       let arr = expandArray(grid.length);
       setGrid(grid.slice().concat(arr));
+      setColumns([...columns, "1fr"]);
     } else {
-      let newGrid = grid.slice(0, grid.length - 5);
-      setGrid(newGrid);
+      setGrid(grid.slice(0, grid.length - 5));
+      setColumns(columns.slice(0, -1));
     }
-    setColumns(num);
   };
 
   const changeRows = e => {
     let num = Number(e.target.value);
-    if (num > rows) {
+    if (num > rows.length) {
       let arr = expandArray(grid.length);
       setGrid(grid.slice().concat(arr));
+      setRows([...rows, "1fr"]);
     } else {
       setGrid(grid.slice(0, grid.length - 5));
+      setRows(rows.slice(0, -1));
     }
-    setRows(num);
   };
 
   const showModal = () => {
@@ -110,9 +122,53 @@ const IndexPage = () => {
     }, 750);
   }, [copyStatus]);
 
+  function determineGrid(item) {
+    let styles = [];
+    let hash = {};
+    for (let style of item) {
+      if (hash[style]) {
+        hash[style]++;
+      } else {
+        hash[style] = 1;
+      }
+    }
+    for (let key in hash) {
+      if (hash[key] > 1) {
+        styles.push(`repeat(${hash[key]}, ${key})`);
+      } else {
+        styles.push(hash[key]);
+      }
+    }
+    return styles.join(" ");
+  }
+
+  const updateColumnValue = (e, i) => {
+    setColumns(
+      columns.map((column, idx) => {
+        if (idx === i) {
+          return e.target.value;
+        } else {
+          return column;
+        }
+      })
+    );
+  };
+
+  const updateRowValue = (e, i) => {
+    setRows(
+      rows.map((row, idx) => {
+        if (idx === i) {
+          return e.target.value;
+        } else {
+          return row;
+        }
+      })
+    );
+  };
+
   let gridStyles = {
-    gridTemplateColumns: `repeat(${columns}, 1fr)`,
-    gridTemplateRows: `repeat(${rows}, 1fr)`,
+    gridTemplateColumns: determineGrid(columns),
+    gridTemplateRows: determineGrid(rows),
     columnGap: `${columnGap}px`,
     rowGap: `${rowGap}px`
   };
@@ -133,12 +189,40 @@ const IndexPage = () => {
       </Modal>
       <Container>
         <Main>
+          <InputGrid
+            style={{ gridTemplateColumns: gridStyles.gridTemplateColumns }}
+          >
+            {columns.map((column, i) => (
+              <input
+                key={i}
+                style={{ maxWidth: 20 }}
+                type="text"
+                onChange={e => updateColumnValue(e, i)}
+                name="column-value"
+                id="column-value"
+                value={column}
+              />
+            ))}
+          </InputGrid>
           <GridContainer>
             <Grid style={gridStyles}>
               {grid.map((_, rowIdx) => {
                 return <Column key={rowIdx} />;
               })}
             </Grid>
+            <InputRow style={{ gridTemplateRows: gridStyles.gridTemplateRows }}>
+              {rows.map((row, i) => (
+                <input
+                  key={i}
+                  style={{ maxWidth: 20 }}
+                  type="text"
+                  onChange={e => updateRowValue(e, i)}
+                  name="row-value"
+                  id="row-value"
+                  value={row}
+                />
+              ))}
+            </InputRow>
           </GridContainer>
         </Main>
         <Aside>
@@ -148,8 +232,9 @@ const IndexPage = () => {
               type="number"
               name="rows"
               onChange={changeRows}
-              value={rows}
+              value={Number(rows.length)}
               id="rows"
+              min="1"
               max="12"
             />
             <label htmlFor="columns">Columns</label>
@@ -157,8 +242,9 @@ const IndexPage = () => {
               type="number"
               name="columns"
               onChange={changeColumns}
-              value={columns}
+              value={Number(columns.length)}
               id="columns"
+              min="1"
               max="12"
             />
             <label htmlFor="column-gap">Column Gap (px)</label>
